bbf60ca1d1467779aae4b39757e5313f8be9fc93
Mark Sherriff <sherriff@virginia.edu>
2022-09-09 09:22:42 -0400

    Commented code

--- app/src/main/java/edu/virginia/cs4720/roomexample/AppRoomDatabase.kt
+++ app/src/main/java/edu/virginia/cs4720/roomexample/AppRoomDatabase.kt
@@ -5,8 +5,17 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-// Annotates class to be a Room Database with a table (entity) of the Word class
+/**
+ * This class represents the database itself.  It identifies the Data Access Object
+ * (DAO) that can be used to read and write data.  It also initializes an empty
+ * database if needed.  Notice the call to .allowMainThreadQueries() in the
+ * creation of the database.  By default, all db queries in Android have to be
+ * done OFF of the main thread to prevent the app hanging, etc.  For testing
+ * purposes (and because I didn't want to deal with launching AsyncTasks),
+ * adding this call overrides that requirement.
+ */
 
+// Annotates class to be a Room Database with a table (entity) of the Word class
 @Database(entities = arrayOf(Student::class), version = 1, exportSchema = false)
 public abstract class AppRoomDatabase : RoomDatabase() {
 
--- app/src/main/java/edu/virginia/cs4720/roomexample/MainActivity.kt
+++ app/src/main/java/edu/virginia/cs4720/roomexample/MainActivity.kt
@@ -18,26 +18,23 @@ import kotlinx.coroutines.launch
  * https://howtodoandroid.com/room-database-android/
  * https://medium.com/huawei-developers/room-database-with-kotlin-mvvm-architecture-477c3ad3c264
  *
- *
+ * Make sure to check out the additions in the build.gradle file!
  */
 
 class MainActivity : AppCompatActivity() {
 
+    // Get a conneciton to the database.  The call of "by lazy" makes it so that
+    // initializing the database doesn't have to hinder the app's normal loading
+    // and it only gets allocated if needed.  Hence lazy initialization.
     private val appDatabase by lazy { AppRoomDatabase.getDatabase(this).studentDao()}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Creating an example student object
         val student1 = Student("mss2x", "Mark")
 
-//        Log.i("RoomAppExample", appDatabase.getStudentListByComputingID().toString())
-//
-//        lifecycleScope.launch {
-//            appDatabase.insert(student1)
-//        }
-//
-//        Log.i("RoomAppExample", appDatabase.getStudentListByComputingID().toString())
     }
 
     fun printDB(view: View) {
@@ -48,6 +45,10 @@ class MainActivity : AppCompatActivity() {
         val compID = findViewById<EditText>(R.id.editTextCompId)
         val name = findViewById<EditText>(R.id.editTextName)
 
+        // lifecycleScope.launch allows me to call a method that has the suspend keyword
+        // which is a coroutine.  Basically, it's kind of like threading, but within
+        // the same thread, and allow some forms of concurrency.  In this case,
+        // we don't want an insert to hold up other things happening.
         lifecycleScope.launch {
             appDatabase.insert(Student(compID.text.toString(), name.text.toString()))
         }
@@ -58,10 +59,13 @@ class MainActivity : AppCompatActivity() {
 }
 
 
-
+// By adding the @Entity annotation here, we are saying that this class should
+// be a table in the database
 @Entity(tableName = "student_table")
 data class Student(@PrimaryKey val computingID: String, val name: String)
 
+// This defines the Data Access Object for the data class above.
+// Effectively it allows you to connect an SQL query to a function call
 @Dao
 interface StudentDao {
 
